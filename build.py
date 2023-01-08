636be6dd85bfe171481f9c406f22999891ff865d
OSmart32 <Owen32Smart@gmail.com>
2023-01-07 23:03:56 -0500

    fix build

--- build.py
+++ build.py
@@ -48,7 +48,7 @@ exe_path = os.path.join(".", "dist", base_name + exe_ext)
 if not (os.path.isfile(exe_path) or os.path.isdir(exe_path)):
 	raise Exception("Executable not found: %s" % exe_path)
 
-release_archive_path = os.path.join(".", "dist", "release_archive_" + VERSION + bitness_suffix)
+release_archive_path = os.path.join(".", "dist", f"release_archive_{VERSION}{bitness_suffix}")
 
 if os.path.exists(release_archive_path) and os.path.isdir(release_archive_path):
 	shutil.rmtree(release_archive_path)
