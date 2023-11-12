72c7c3c1665228db99a74f75b810373a1409269f
OSmart32 <Owen32Smart@gmail.com>
2023-11-12 02:00:47 -0500

    clean main script a little

--- randomizer.py
+++ randomizer.py
@@ -2,11 +2,10 @@
 
 from PySide6 import QtCore, QtGui, QtWidgets
 import UI.main_window as window
-from randomizer_paths import RESOURCE_PATH
+from randomizer_paths import RESOURCE_PATH, IS_RUNNING_FROM_SOURCE
 
 import os
 import sys
-
 
 def interruptHandler(sig, frame):
     sys.exit(0)
@@ -15,11 +14,8 @@ def interruptHandler(sig, frame):
 import signal
 signal.signal(signal.SIGINT, interruptHandler)
 
-
-# Test if code is being ran from a build, and if not, set app id so the custom taskbar icon will show while running from source
-try:
-    from sys import _MEIPASS
-except ImportError:
+# Set app id so the custom taskbar icon will show while running from source
+if IS_RUNNING_FROM_SOURCE:
     from ctypes import windll
     try:
         windll.shell32.SetCurrentProcessExplicitAppUserModelID("Link's_Awakening_Switch_Randomizer")
