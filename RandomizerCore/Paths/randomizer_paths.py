6e961ba6892e2e95ea22408f31f08f2fd776c879
Owen Smart <Owen32Smart@gmail.com>
2025-07-31 06:28:17 -0400

    change paths to use pathlib Path

--- RandomizerCore/Paths/randomizer_paths.py
+++ RandomizerCore/Paths/randomizer_paths.py
@@ -1,31 +1,29 @@
-import os
-import sys
-import appdirs
-import platform
+from pathlib import Path
+import appdirs, platform, sys
 
 # check if user is running a precompiled binary
 if getattr(sys, "frozen", False):
     IS_RUNNING_FROM_SOURCE = False
-    ROOT_PATH = os.path.dirname(sys.executable)
-    ASM_PATH = os.path.join(ROOT_PATH, 'lib/RandomizerCore/ASM/Patches')
-    DATA_PATH = os.path.join(ROOT_PATH, 'Data')
-    RESOURCE_PATH = os.path.join(ROOT_PATH, 'Resources')
+    ROOT_PATH = Path(sys.executable).parent
+    ASM_PATH = ROOT_PATH / 'lib/RandomizerCore/ASM/Patches'
+    DATA_PATH = ROOT_PATH / 'Data'
+    RESOURCE_PATH = ROOT_PATH / 'Resources'
     if platform.system() == 'Darwin':
-        userdata_path = appdirs.user_data_dir('randomizer', 'LAS Randomizer')
-        if not os.path.isdir(userdata_path):
-            os.mkdir(userdata_path)
-        SETTINGS_PATH = os.path.join(userdata_path, 'settings.txt')
-        LOGS_PATH = os.path.join(userdata_path, 'log.txt')
+        userdata_path = Path(appdirs.user_data_dir('randomizer', 'LAS Randomizer'))
+        if not userdata_path.is_dir():
+            userdata_path.mkdir(parents=True, exist_ok=True)
+        SETTINGS_PATH = userdata_path / 'settings.txt'
+        LOGS_PATH = userdata_path / 'log.txt'
     else:
-        SETTINGS_PATH = os.path.join('.', 'settings.txt')
-        LOGS_PATH = os.path.join('.', 'log.txt')
+        SETTINGS_PATH = ROOT_PATH / 'settings.txt'
+        LOGS_PATH = ROOT_PATH / 'log.txt'
 else:
     IS_RUNNING_FROM_SOURCE = True
-    ROOT_PATH = os.path.dirname(sys.argv[0])
-    ASM_PATH = os.path.join(ROOT_PATH, 'RandomizerCore/ASM/Patches')
-    DATA_PATH = os.path.join(ROOT_PATH, 'RandomizerCore/Data')
-    RESOURCE_PATH = os.path.join(ROOT_PATH, 'RandomizerUI/Resources')
-    SETTINGS_PATH = os.path.join(ROOT_PATH, 'settings.txt')
-    LOGS_PATH = os.path.join(ROOT_PATH, 'log.txt')
+    ROOT_PATH = Path(sys.argv[0]).parent
+    ASM_PATH = ROOT_PATH / 'RandomizerCore' / 'ASM' / 'Patches'
+    DATA_PATH = ROOT_PATH / 'RandomizerCore' / 'Data'
+    RESOURCE_PATH = ROOT_PATH / 'RandomizerUI' / 'Resources'
+    SETTINGS_PATH = ROOT_PATH / 'settings.txt'
+    LOGS_PATH = ROOT_PATH / 'log.txt'
 
-LOGIC_PATH = os.path.join(DATA_PATH, 'logic.yml')
+LOGIC_PATH = DATA_PATH / 'logic.yml'
