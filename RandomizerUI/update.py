6f3f666146719024cd817e36aeee6c6ea415920a
Owen Smart <Owen32Smart@gmail.com>
2025-08-02 20:30:57 -0400

    updated constant name

--- RandomizerUI/update.py
+++ RandomizerUI/update.py
@@ -1,31 +1,29 @@
 from PySide6 import QtCore
 import urllib.request as lib
-
-from RandomizerCore.randomizer_data import VERSION, LOGIC_VERSION
+from RandomizerCore.randomizer_data import APP_VERSION, LOGIC_VERSION
 
 
 
 class UpdateProcess(QtCore.QThread):
     can_update = QtCore.Signal(bool)
     give_version = QtCore.Signal(float)
-    
 
     def __init__(self, parent=None):
         QtCore.QThread.__init__(self, parent)
-    
-    
+
+
     def run(self):
         try:
             update_file =\
                 lib.urlopen("https://raw.githubusercontent.com/Owen-Splat/LAS-Randomizer/master/version.txt")
             web_version = float(update_file.read().strip())
-            
-            if web_version > VERSION:
+
+            if web_version > APP_VERSION:
                 self.give_version.emit(web_version)
                 self.can_update.emit(True)
             else:
                 self.can_update.emit(False)
-        
+
         except Exception:
             self.can_update.emit(False)
 
@@ -34,13 +32,12 @@ class UpdateProcess(QtCore.QThread):
 class LogicUpdateProcess(QtCore.QThread):
     can_update = QtCore.Signal(bool)
     give_logic = QtCore.Signal(tuple)
-    
-    
+
     def __init__(self, parent=None, ver=LOGIC_VERSION):
         QtCore.QThread.__init__(self, parent)
         self.ver = ver
-    
-    
+
+
     def run(self):
         try:
             update_file =\
@@ -53,6 +50,6 @@ class LogicUpdateProcess(QtCore.QThread):
                 self.can_update.emit(True)
             else:
                 self.can_update.emit(False)
-        
+
         except Exception:
             self.can_update.emit(False)
