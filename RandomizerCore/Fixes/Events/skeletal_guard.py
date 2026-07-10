bcfa20d9a65e1d625886606bb0eac3c21d6b697c
Owen_Splat <Owen32Smart@gmail.com>
2026-07-10 13:34:16 -0400

    remove color dungeon guard selling bombs

--- RandomizerCore/Fixes/Events/skeletal_guard.py
+++ RandomizerCore/Fixes/Events/skeletal_guard.py
@@ -10,24 +10,11 @@ class SkeletalGuardEventFixes:
             # edit Magic Powder amount from 20 to 40 so that it'll max even with the capacity upgrade
             event_tools.findEvent(flow.flowchart, 'Event19').data.params.data['count'] = 40
 
-            # give 60 Bombs so that it'll max even with the capacity upgrade
-            add_bombs = event_tools.createActionEvent(flow.flowchart, 'Inventory', 'AddItem',
-                {'itemType': 4, 'count': 60, 'autoEquip': False})
-
             # check GetMagicPowder flag before buying
-            # these guards will no longer be a source for getting your main powder, and cannot sell bombs until the player can buy powder
+            # these guards will no longer be a source for getting your main powder
             if mod_generator.settings["Shuffled Powder"]:
                 check_powder = event_tools.createSwitchEvent(flow.flowchart, 'EventFlags', 'CheckFlag',
                     {'symbol': 'GetMagicPowder'}, {0: 'Event54', 1: 'Event46'})
                 event_tools.setSwitchEventCase(flow.flowchart, 'Event7', 1, check_powder)
-
-            # check BombsFound flag when buying powder so we can give some additional resources if available
-            # these guards are not a source for getting your main bombs
-            if mod_generator.settings["Shuffled Bombs"]:
-                check_bombs = event_tools.createSwitchEvent(flow.flowchart, 'EventFlags', 'CheckFlag',
-                    {'symbol': mod_generator.flag_manager.flags["BombsFoundFlag"]}, {0: None, 1: add_bombs})
-                event_tools.insertEventAfter(flow.flowchart, 'Event19', check_bombs)
-            else:
-                event_tools.insertEventAfter(flow.flowchart, 'Event19', add_bombs)
 
             mod_generator.file_manager.writeFile('SkeletalGuardBlue.bfevfl', flow)
